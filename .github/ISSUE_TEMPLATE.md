a9f6468a62c818cf33c1e33390371ffdf66965ed
floaks <info@floaks.com>
2017-12-08 13:21:11 +0100

    Windows, MacOS and Linux wallet

---
+++ .github/ISSUE_TEMPLATE.md
@@ -0,0 +1,25 @@
+<!--
+Thanks for opening an issue! A few things to keep in mind:
+
+- Before reporting a bug, please try reproducing your issue with the latest version.
+- Please verify that the bug is related to the Floaks.Wallet Desktop app, and NOT the
+  main web app by testing in Chrome/Firefox.
+- If the issue occurs in the browser, report to github.com/Floaks/Floaks.Wallet instead
+-->
+
+## My Setup
+- Operating System:
+- App Version:
+
+<!-- Answer questions by putting x in box, e.g. [x] -->
+- [ ] I have tested with the latest version
+- [ ] I can simulate the issue easily
+
+## Description
+<!-- If relevant, attach screenshots, and how to reproduce -->
+
+### Current Behavior
+<!-- What actually happens? -->
+
+### Expected Behavior
+<!-- What do you think should happen? -->
---
+++ src/scripts/menus/help.js
@@ -0,0 +1,31 @@
+import { remote } from 'electron';
+import servers from '../servers';
+import i18n from '../../i18n/index.js';
+const APP_NAME = remote.app.getName();
+
+const helpTemplate = [
+    {
+        label: i18n.__('Help_Name', APP_NAME),
+        click: () => remote.shell.openExternal('https://floaks.wallet/docs')
+    },
+    {
+        type: 'separator'
+    },
+    {
+        label: i18n.__('Report_Issue'),
+        click: () => remote.shell.openExternal('https://github.com/floaks/Floaks.Wallet/issues')
+    },
+    {
+        label: i18n.__('Reset_App_Data'),
+        click: () => servers.resetAppData()
+    },
+    {
+        type: 'separator'
+    },
+    {
+        label: i18n.__('Learn_More'),
+        click: () => remote.shell.openExternal('https://floaks.com')
+    }
+];
+
+export default helpTemplate;
